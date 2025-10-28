cd7eafdc2feeb64be272bf70ab5c6c8cce28b212
Java-Dev1 <devendrareddy986@gmail.com>
2025-10-28 11:36:34 +0530

    test: should return sum of two comma-separated numbers

--- string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
+++ string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
@@ -15,6 +15,13 @@ public class StringCalculatorTest {
 	void shouldReturnNumberWhenSingleNumberGiven() {
 		StringCalculator calc = new StringCalculator();
 		assertEquals(5, calc.add("5"));
+
+	}
+
+	@Test
+	void shouldReturnSumOfTwoNumbers() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals(6, calc.add("1,5"));
 	}
 
 }
