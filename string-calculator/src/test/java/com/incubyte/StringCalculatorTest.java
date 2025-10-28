9669a344a4a2bb30bd14d806a567627b5345bba4
Java-Dev1 <devendrareddy986@gmail.com>
2025-10-28 11:17:46 +0530

    test: should return 0 for empty string

---
+++ string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.incubyte;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+	@Test
+	void shouldReturnZeroForEmptyString() {
+		StringCalculator calc  = new StringCalculator();
+		assertEquals(0, calc.add(""));
+	}
+
+}
