07686a70fadf1d26681ee93d8dc95f6228c1728e
Java-Dev1 <devendrareddy986@gmail.com>
2025-10-28 11:41:09 +0530

    test: should handle newline between numbers

--- string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
+++ string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
@@ -24,4 +24,10 @@ public class StringCalculatorTest {
 		assertEquals(6, calc.add("1,5"));
 	}
 
+	@Test
+	void shouldHandleNewLinesBetweenNumbers() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals(6, calc.add("1\n2,3"));
+	}
+
 }
